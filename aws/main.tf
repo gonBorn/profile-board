68ce0a32baa7f58c4c10a4647ac9b0b7b112e8d9
Zeyan <zeyan.du@rea-group.com>
2025-08-18 00:17:37 +0800

    Sperate create DB operation from terraform apply and add api gateway

--- aws/main.tf
+++ aws/main.tf
@@ -50,6 +50,43 @@ resource "aws_instance" "profile_board_ec2" {
   }
 }
 
+resource "aws_api_gateway_rest_api" "profile_board_api" {
+  name        = "profile-board-api"
+  description = "API Gateway for profile board heartbeat endpoint"
+}
+
+resource "aws_api_gateway_resource" "heartbeat" {
+  rest_api_id = aws_api_gateway_rest_api.profile_board_api.id
+  parent_id   = aws_api_gateway_rest_api.profile_board_api.root_resource_id
+  path_part   = "heartbeat"
+}
+
+resource "aws_api_gateway_method" "heartbeat_get" {
+  rest_api_id   = aws_api_gateway_rest_api.profile_board_api.id
+  resource_id   = aws_api_gateway_resource.heartbeat.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "heartbeat_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.profile_board_api.id
+  resource_id             = aws_api_gateway_resource.heartbeat.id
+  http_method             = aws_api_gateway_method.heartbeat_get.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP"
+  uri                     = "http://${aws_instance.profile_board_ec2.public_ip}:8080/heartbeat"
+}
+
+resource "aws_api_gateway_deployment" "profile_board_api_deployment" {
+  depends_on = [aws_api_gateway_integration.heartbeat_integration]
+  rest_api_id = aws_api_gateway_rest_api.profile_board_api.id
+  stage_name  = "prod"
+}
+
+output "api_gateway_invoke_url" {
+  value = "https://${aws_api_gateway_rest_api.profile_board_api.id}.execute-api.${var.region}.amazonaws.com/prod/heartbeat"
+}
+
 variable "db_password" {
   description = "The password for the RDS database."
   type        = string
